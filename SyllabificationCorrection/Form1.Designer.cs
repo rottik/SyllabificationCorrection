8e6417961fe95987cf322319ff37edb9a09e036d
Michal Rott <rott.michal@gmail.com>
2017-09-15 15:48:51 +0200

    first commit

---
+++ SyllabificationCorrection/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace SyllabificationCorrection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonFix = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonAbbrevation = new System.Windows.Forms.Button();
+            this.buttonForeign = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.LabelEdit = true;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.ShowPlusMinus = false;
+            this.treeView1.Size = new System.Drawing.Size(499, 350);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.Location = new System.Drawing.Point(390, 3);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(106, 40);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonFix
+            // 
+            this.buttonFix.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonFix.Location = new System.Drawing.Point(279, 3);
+            this.buttonFix.Name = "buttonFix";
+            this.buttonFix.Size = new System.Drawing.Size(105, 40);
+            this.buttonFix.TabIndex = 2;
+            this.buttonFix.Text = "FIX";
+            this.buttonFix.UseVisualStyleBackColor = true;
+            this.buttonFix.Click += new System.EventHandler(this.buttonFix_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(499, 447);
+            this.splitContainer1.SplitterDistance = 350;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 316);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 53F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonFix, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonChange, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonOK, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonAbbrevation, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonForeign, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(499, 93);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonChange.Location = new System.Drawing.Point(3, 49);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(217, 41);
+            this.buttonChange.TabIndex = 4;
+            this.buttonChange.Text = "Change Syllables";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.buttonChange_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(3, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 29);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonAbbrevation
+            // 
+            this.buttonAbbrevation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAbbrevation.Location = new System.Drawing.Point(279, 49);
+            this.buttonAbbrevation.Name = "buttonAbbrevation";
+            this.buttonAbbrevation.Size = new System.Drawing.Size(105, 41);
+            this.buttonAbbrevation.TabIndex = 5;
+            this.buttonAbbrevation.Text = "Abbreviation";
+            this.buttonAbbrevation.UseVisualStyleBackColor = true;
+            this.buttonAbbrevation.Click += new System.EventHandler(this.buttonAbbrevation_Click);
+            // 
+            // buttonForeign
+            // 
+            this.buttonForeign.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonForeign.Location = new System.Drawing.Point(390, 49);
+            this.buttonForeign.Name = "buttonForeign";
+            this.buttonForeign.Size = new System.Drawing.Size(106, 41);
+            this.buttonForeign.TabIndex = 6;
+            this.buttonForeign.Text = "Foreign";
+            this.buttonForeign.UseVisualStyleBackColor = true;
+            this.buttonForeign.Click += new System.EventHandler(this.buttonForeign_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(226, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 447);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Corrector for JSON syllabic dictionary";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonFix;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonAbbrevation;
+        private System.Windows.Forms.Button buttonForeign;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SyllabificationCorrection/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace SyllabificationCorrection
+{
+    public partial class Form1 : Form
+    {
+        private List<Slovo> words;
+        List<Slovo> OKwords = new List<Slovo>();
+        List<Slovo> correctedWords = new List<Slovo>();
+        List<Slovo> abbrevList = new List<Slovo>();
+        List<Slovo> ForeingList = new List<Slovo>();
+        Slovo showedWord;
+        string dataFile;
+
+        public Slovo ShowedWord
+        {
+            get { return showedWord; }
+            set { showedWord = value; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.Filter = "PCS Files (.pcs)|*.pcs|JSON Files (.json)|*.json|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                dataFile = openFileDialog1.FileName;
+                if (dataFile.EndsWith(".json"))
+                    words = ParseJson(dataFile);
+                if (dataFile.EndsWith(".pcs"))
+                    words = ParsePCS(dataFile);
+
+                //SaveToPCS(dataFile.Replace("json", "pcs"));
+
+                if (MessageBox.Show("Load previous work?", "Load previous work?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    if (File.Exists(dataFile + ".okWords"))
+                        OKwords = ParsePCS(dataFile + ".okWords");
+                    if (File.Exists(dataFile + ".correctedWords"))
+                        correctedWords = ParsePCS(dataFile + ".correctedWords");
+                    if (File.Exists(dataFile + ".foreignWords"))
+                        ForeingList = ParsePCS(dataFile + ".foreignWords");
+                    if (File.Exists(dataFile + ".abbrevationWords"))
+                        abbrevList = ParsePCS(dataFile + ".abbrevationWords");
+                    //MessageBox.Show("Not working!");
+
+                    List<Slovo> remove = OKwords.Union(correctedWords).Union(ForeingList).Union(abbrevList).ToList();
+                    words = words.Where(p=>!remove.Contains(p)).ToList(); ;
+                }
+                showedWord = words.First();
+                ShowWord(showedWord);
+            }
+            else
+                this.Close();
+
+        }
+
+        private void SaveToPCS(string fileName)
+        {
+            SaveToPCS(fileName, this.words);
+        }
+
+        private void SaveToPCS(string fileName, List<Slovo> words)
+        {
+            if (words.Count == 0)
+                return;
+            List<Slovo> empty = new List<Slovo>();
+            TextWriter tw = new StreamWriter(fileName);
+            tw.WriteLine("==========");
+            foreach (Slovo word in words)
+            {
+                try
+                {
+                    tw.WriteLine(word.Ortho + "\t" + word.Phone.Keys.First());
+                    foreach (string s in word.Phone[word.Phone.Keys.First()])
+                        tw.WriteLine("\t" + s);
+                    tw.WriteLine("==========");
+                }
+                catch (Exception e)
+                {
+                    empty.Add(word);
+                }
+            }
+            tw.Close();
+            if(empty.Count>0)
+                File.WriteAllLines(fileName + ".errors", empty.Select(p => p.Ortho));
+        }
+
+        private List<Slovo> ParsePCS(string file)
+        {
+            string[] wordParts = File.ReadAllText(file).Trim().Split(new string[] { "==========" }, StringSplitOptions.RemoveEmptyEntries);
+            List<Slovo> ws = new List<Slovo>();
+            Progress progresForm = new Progress();
+            progresForm.Show();
+            int cnt = 0;
+            int step = wordParts.Length / progresForm.progressBar1.Maximum;
+            if (step == 0) step = 1;
+            foreach (string part in wordParts)
+            {
+                string ortho = "";
+                string ortSyl = "";
+                Dictionary<string, List<string>> phns = new Dictionary<string, List<string>>();
+                foreach (string line in part.Split(new string[] { "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    if (!line.StartsWith("\t"))
+                    {
+                        ortho = line.Split('\t')[0];
+                        ortSyl = line.Split('\t')[1];
+                        phns.Add(ortSyl, new List<string>());
+                    }
+                    else
+                        phns[ortSyl].Add(line.Trim());
+                }
+
+                Slovo s = new Slovo(ortho, phns);
+                ws.Add(s);
+                cnt++;
+                if (cnt % step == 0)
+                {
+                    progresForm.progressBar1.Value++;
+                }
+            }
+            progresForm.Close();
+            return ws;
+        }
+
+
+
+        private void ShowWord(Slovo w)
+        {
+            treeView1.Nodes.Clear();
+            foreach (string key in w.Phone.Keys)
+            {
+                label1.Text = w.Ortho;
+                string ortSyl = key;
+
+                List<TreeNode> tnList = new List<TreeNode>();
+                foreach (string phn in w.Phone[ortSyl])
+                {
+                    TreeNode tnp = new TreeNode("phn: " + phn);
+                    tnList.Add(tnp);
+                }
+
+                TreeNode n = new TreeNode("ort: " + key, tnList.ToArray());
+                n.ExpandAll();
+
+                treeView1.Nodes.Add(n);
+            }
+        }
+
+        private static List<Slovo> ParseJson(string fileJson)
+        {
+            List<Slovo> slova = new List<Slovo>();
+            string content = Regex.Unescape(File.ReadAllText(fileJson)).Substring(1);
+            string[] parts = content.Split(new string[] { @"}," }, StringSplitOptions.RemoveEmptyEntries);
+            Regex ortho = new Regex("\"([\\p{L}\\p{N}]+)\"\\:\\s\\{");
+            Regex phone = new Regex("([\\p{L}\\p{N}]+)\":\\s\\[(\\[.*?\\])\\]");
+            Regex syllabsReg = new Regex("\"([\\p{L}\\p{N}]+)\",\\s+\"([\\p{L}\\p{N}]+)\"");
+            Progress progresForm = new Progress();
+            progresForm.Show();
+            int cnt = 0;
+            int step = parts.Length / 100;
+            if (step == 0) step = 1;
+            foreach (string line in parts)
+            {
+                cnt++;
+                Match m = ortho.Match(line);
+                string lineTmp = line.Substring(m.Index + m.Length);
+                MatchCollection mPHs = phone.Matches(lineTmp);
+                Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+                foreach (Match mc in mPHs)
+                {
+                    Console.WriteLine();
+                    string syls = mc.Groups[2].ToString();
+                    MatchCollection mc2 = syllabsReg.Matches(syls);
+                    List<Tuple<string, string>> sylabs = new List<Tuple<string, string>>();
+                    foreach (Match mxx in mc2)
+                    {
+                        Tuple<string, string> tp = new Tuple<string, string>(mxx.Groups[1].ToString(), mxx.Groups[2].ToString());
+                        sylabs.Add(tp);
+                    }
+
+                    StringBuilder sbORTSyl = new StringBuilder();
+                    foreach (string s in sylabs.Select(p => p.Item1))
+                        sbORTSyl.Append(s + "-");
+                    string ortSyl = sbORTSyl.Remove(sbORTSyl.Length - 1, 1).ToString();
+                    if (!dic.ContainsKey(ortSyl))
+                        dic.Add(ortSyl, new List<string>());
+
+                    StringBuilder sbPHNSyl = new StringBuilder();
+                    foreach (string s in sylabs.Select(p => p.Item2))
+                        sbPHNSyl.Append(s + "-");
+                    string phnSyl = sbPHNSyl.Remove(sbPHNSyl.Length - 1, 1).ToString();
+                    dic[ortSyl].Add(phnSyl);
+
+                    //dic.Add(mc.Groups[1].ToString(), sylabs);
+                }
+                Slovo w = new Slovo(m.Groups[1].ToString(), dic);
+                slova.Add(w);
+                if (cnt % step == 0)
+                {
+                    progresForm.progressBar1.Value++;
+                }
+            }
+            progresForm.Close();
+            return slova; ;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            if (treeView1.Nodes.Count == 0) return;
+            OKwords.Add(showedWord);
+            LoadNewWord();
+        }
+
+        private void LoadNewWord()
+        {
+            List<Slovo> remove = OKwords.Union(correctedWords).Union(ForeingList).Union(abbrevList).ToList();
+            words = words.Except(remove).ToList();
+            if (words.Count != 0)
+            {
+                showedWord = words.First();
+                ShowWord(showedWord);
+            }
+            else
+            {
+                MessageBox.Show("Nothing to show!");
+                treeView1.Nodes.Clear();
+            }
+        }
+
+        private void buttonFix_Click(object sender, EventArgs e)
+        {
+            if (treeView1.Nodes.Count == 0) return;
+            Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+            foreach (TreeNode node in treeView1.Nodes)
+            {
+                List<string> phnList = new List<string>();
+                foreach (TreeNode PHNnd in node.Nodes)
+                {
+                    phnList.Add(PHNnd.Text.Replace("phn: ",""));
+                }
+                dic.Add(node.Text.Replace("ort: ",""), phnList);
+            }
+            showedWord.Phone = dic;
+            correctedWords.Add(showedWord);
+            LoadNewWord();
+        }
+
+        private bool ort = false;
+        private bool phn = false;
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            ort = false;
+            phn = false;
+            if (treeView1.SelectedNode.Text.Contains("ort: "))
+                ort = true;
+            if (treeView1.SelectedNode.Text.Contains("phn: "))
+                phn = true;
+            textBox1.Text = treeView1.SelectedNode.Text.Replace("ort: ", "").Replace("phn: ", "");
+        }
+
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+            string prefix = "";
+            if (ort) prefix = "ort: ";
+            if (phn) prefix = "phn: ";
+            treeView1.SelectedNode.Text = prefix + textBox1.Text.Trim();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Save", "Save", MessageBoxButtons.YesNoCancel);
+            if (res == DialogResult.Yes)
+            {
+                SaveToPCS(dataFile + ".okWords", OKwords);
+                SaveToPCS(dataFile + ".correctedWords", correctedWords);
+                SaveToPCS(dataFile + ".foreignWords", ForeingList);
+                SaveToPCS(dataFile + ".abbrevationWords", abbrevList);
+            }
+            else if (res == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void buttonForeign_Click(object sender, EventArgs e)
+        {
+            if (treeView1.Nodes.Count == 0) return;
+            ForeingList.Add(showedWord);
+            LoadNewWord();
+        }
+
+        private void buttonAbbrevation_Click(object sender, EventArgs e)
+        {
+            if (treeView1.Nodes.Count == 0) return;
+            abbrevList.Add(showedWord);
+            LoadNewWord();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveToPCS(dataFile + ".okWords", OKwords);
+            SaveToPCS(dataFile + ".correctedWords", correctedWords);
+            SaveToPCS(dataFile + ".foreignWords", ForeingList);
+            SaveToPCS(dataFile + ".abbrevationWords", abbrevList);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Slovo
+    {
+        string ortho;
+
+        public override bool Equals(Object obj)
+        {
+            Slovo other = obj as Slovo;
+            if (other == null) return false;
+            if (other.Ortho == this.Ortho)
+                return true;
+            return false;
+        }
+
+
+        public string Ortho
+        {
+            get { return ortho; }
+        }
+        Dictionary<string, List<string>> phone = new Dictionary<string, List<string>>();
+
+        public Dictionary<string, List<string>> Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public Slovo(string _ortho, Dictionary<string, List<string>> _phone)
+        {
+            ortho = _ortho;
+            phone = _phone;
+        }
+
+    }
+}
